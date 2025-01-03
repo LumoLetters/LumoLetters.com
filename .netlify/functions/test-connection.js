eff8f10223be5c447c1d09182d8413ab73b9cfd3
BenLarios <88693357+BenLarios@users.noreply.github.com>
2025-01-03 14:09:21 -0800

    testing DB

--- .netlify/functions/test-connection.js
+++ .netlify/functions/test-connection.js
@@ -5,23 +5,19 @@ const client = new MongoClient(uri);
 
 exports.handler = async (event, context) => {
   try {
-    
      console.log("Attempting to connect")
     await client.connect();
     console.log("Successfully Connected");
-    
-     return {
-      statusCode: 200,
-       body: JSON.stringify({message: "Connection Successful!"}),
-    };
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Failed to connect to MongoDB", message: error.message }),
-    };
-  } finally {
-     console.log("Closing Connection");
-    await client.close();
-  }
+
+        return {
+            statusCode: 200,
+           body: JSON.stringify({message: "Connection Successful!"}),
+        };
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed to connect to MongoDB", message: error.message }),
+        };
+   }
 };
