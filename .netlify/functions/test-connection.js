ff6a561d2cec44fdfb9829681cce4c574d28af39
BenLarios <88693357+BenLarios@users.noreply.github.com>
2025-01-03 19:29:13 -0800

    testing MongoDB

--- .netlify/functions/test-connection.js
+++ .netlify/functions/test-connection.js
@@ -8,16 +8,23 @@ exports.handler = async (event, context) => {
      console.log("Attempting to connect")
     await client.connect();
     console.log("Successfully Connected");
+       const db = client.db("lumoletters");
+      const collection = db.collection("test-collection"); // Replace with your collection name if any
+    const testDoc = await collection.findOne({});
+   console.log("found a document:", testDoc)
 
-        return {
-            statusCode: 200,
-           body: JSON.stringify({message: "Connection Successful!"}),
-        };
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: "Failed to connect to MongoDB", message: error.message }),
-        };
-   }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: "Connection successful!", data: testDoc }),
+    };
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to connect to MongoDB", message: error.message }),
+    };
+  } finally {
+    console.log("Closing Connection");
+    await client.close();
+  }
 };
