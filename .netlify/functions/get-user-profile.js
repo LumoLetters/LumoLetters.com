a542043d5b51118dbed35c0a49436629819a69f0
BenLarios <88693357+BenLarios@users.noreply.github.com>
2025-01-04 20:26:58 -0800

    added mongoose for mongoDB

--- .netlify/functions/get-user-profile.js
+++ .netlify/functions/get-user-profile.js
@@ -1,30 +1,42 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
     
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
+   
+ const UserProfile = mongoose.model('UserProfile', new mongoose.Schema({
+    userId: {type: String, unique: true, required: true},
+    name: String,
+     dob: String,
+    email: String,
+    address: String,
+    city: String,
+    state: String,
+    zip: String,
+      interests: [String],
+    topics: [String],
+    signature: String,
+    date: String,
+}, {timestamps: true}));
 
 exports.handler = async (event, context) => {
     try {
-        console.log("Attempting to connect")
-        await client.connect();
-        console.log("Successfully Connected");
-        const db = client.db("lumoletters");
-        const collection = db.collection("user-profiles");
+       console.log("Attempting to Connect to Mongoose")
+        await mongoose.connect(uri);
+        console.log("Successfully Connected to Mongoose");
+         const userId = context.clientContext.user.sub;
 
-       const doc = await collection.findOne({ userId: context.clientContext.user.sub });
-
+      const doc = await UserProfile.findOne({ userId });
         return {
-          statusCode: 200,
-          body: JSON.stringify({ message: "Successfully got user profile", data: doc}),
-         };
+            statusCode: 200,
+           body: JSON.stringify({ message: "Successfully got user profile", data: doc }),
+        };
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
         return {
             statusCode: 500,
            body: JSON.stringify({ error: "Failed to connect to MongoDB", message: error.message }),
-       };
+        };
    } finally {
-       console.log("Closing Connection");
-      await client.close();
-  }
+       console.log("Disconnecting");
+      await mongoose.disconnect();
+    }
 };
