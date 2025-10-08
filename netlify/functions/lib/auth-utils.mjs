dfbcbecc6ef565a5323fdee583c4f300cec794d2
BenLarios <88693357+BenLarios@users.noreply.github.com>
2025-10-08 05:57:37 -0700

    auth0 logging

--- netlify/functions/lib/auth-utils.mjs
+++ netlify/functions/lib/auth-utils.mjs
@@ -45,6 +45,13 @@ function getKey(header, callback) {
 
 export async function verifyTokenAndGetSub(event) {
   // Extract token from Authorization header
+
+  console.log('Auth0 ENV', {
+  AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+  AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE
+});
   const authHeader = event.headers.authorization || event.headers.Authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
